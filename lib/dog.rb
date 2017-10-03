9499cc3fde8d8ff09eca3d4f912a3f024cd63286
Monica  Karasek <mlkarasek@gmail.com>
2017-10-03 16:30:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,16 +4,3 @@ def initialize(name, breed)
   @name = name
   @breed = breed
 end
-def breed
-  @breed 
-
-def name
-  @name
-end
-
-def name=(new_name)
-  @name = new_name
-end
-end
-
-fido = Dog.new
