95af8698fab346d4bb2c0d43bdbed591ff10e8f5
Monica  Karasek <mlkarasek@gmail.com>
2017-10-03 17:06:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,15 @@
 class Dog
 
-def initialize(dogs_name, dogs_breed)
+def name=(dogs_name)
   @this_dogs_name = dogs_name
-  @this_dogs_breed = dogs_breed
-end
-
-def name=(dogs_name, dogs_breed)
-  @this_dogs_name = dogs_name
-  @this_dogs_breed = dogs_breed
 end
 
 def name
   @this_dogs_name
 end
+
+def breed(dogs_breed)
+@this_dogs_breed = dogs_breed
 
 def breed
   @this_dogs_breed
